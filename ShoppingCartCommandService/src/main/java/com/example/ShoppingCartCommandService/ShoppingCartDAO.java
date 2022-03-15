750a3ad4650bbe4be837206e20c0ebdc9b5c731a
Allan Muhwezi <muhallan1@gmail.com>
2022-03-14 20:39:29 -0500

    delete ShoppingCartCommandService and .idea

--- ShoppingCartCommandService/src/main/java/com/example/ShoppingCartCommandService/ShoppingCartDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.ShoppingCartCommandService;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShoppingCartDAO extends MongoRepository<ShoppingCart, Long> {
-}
