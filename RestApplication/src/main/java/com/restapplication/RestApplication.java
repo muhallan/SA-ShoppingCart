fad9578b5c0d39964d24c36163440fc19041a447
SuThwe <suthwethwetun@gmail.com>
2022-03-16 14:03:59 -0500

    changed productNumber

--- RestApplication/src/main/java/com/restapplication/RestApplication.java
+++ RestApplication/src/main/java/com/restapplication/RestApplication.java
@@ -30,7 +30,7 @@ public class RestApplication implements CommandLineRunner {
         /**** 1. Add a number of products in the product service ****/
         //add product1
         restTemplate.postForLocation(productUrl, new Product(
-                "product1",
+                "1",
                 "1",
                 10000.0,
                 "First product",
@@ -47,26 +47,32 @@ public class RestApplication implements CommandLineRunner {
         ));
 
         // get product1
-        Product product1 = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "product1");
+        Product product1 = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "1");
         System.out.println("----------- get product1-----------------------");
         System.out.println(product1);
         // get product2
-        Product product2 = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "product2");
+        /*Product product2 = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "2");
         System.out.println("----------- get product2-----------------------");
-        System.out.println(product2);
+        System.out.println(product2);*/
+
+
 
         /**** 2. Modify a product in the productservice ****/
         //modify product1
         product1.setNumberInStock(200);
         restTemplate.put(productUrl + "/{productNumber}", product1, "product1");
 
+
+
         /**** 3. Retrieve one or more products and show these products ****/
         //get modified product
-        Product modifyProduct = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "product1");
-        System.out.println("----------- get modifyProduct1-----------------------");
+        Product modifyProduct = restTemplate.getForObject(productUrl + "/{productNumber}", Product.class, "1");
+        System.out.println("----------- get modifyProduct1 (numberInStock changed) -----------------------");
         System.out.println(modifyProduct);
 
-        // add customer
+
+
+//        // add customer
         Customer customer1 = new Customer(
                 "customer1",
                 "Suzy",
@@ -79,10 +85,12 @@ public class RestApplication implements CommandLineRunner {
         );
         restTemplate.postForLocation(customerUrl + "/add", customer1);
 
-        /*//getCustomer
+        //getCustomer
         Customer customer = restTemplate.getForObject(customerUrl + "/{cId}", Customer.class, "customer1");
         System.out.println("----------- get customer1-----------------------");
-        System.out.println(customer);*/
+        System.out.println(customer);
+
+
 
         /**** 10. Add customer to order ****/
         //create shopping cart for customer1
@@ -90,43 +98,57 @@ public class RestApplication implements CommandLineRunner {
         ShoppingCartCustomer shoppingCartCustomer = new ShoppingCartCustomer(customer1.getcId(), shoppingCartNumber);
         restTemplate.postForLocation(shoppingCartUrl, shoppingCartCustomer);
 
+
+
         /**** 4. Put some products in the shoppingcart  ****/
         //put product to shopping cart
-        ShoppingCartProduct shoppingCartProduct = new ShoppingCartProduct(
-                Long.parseLong(product1.getProductNumber()),
-                product1.getNumberInStock(),
-                product1.getPrice());
-        restTemplate.postForLocation(shoppingCartUrl + "/{cartId}/products", shoppingCartProduct, shoppingCartCustomer);
+//        ShoppingCartProduct shoppingCartProduct = new ShoppingCartProduct(
+//                Long.parseLong(product1.getProductNumber()),
+//                product1.getNumberInStock(),
+//                product1.getPrice());
+//        restTemplate.postForLocation(shoppingCartUrl + "/{cartId}/products", shoppingCartProduct, shoppingCartCustomer);
+
+
 
         /**** 5. Retrieve and show the shoppingcart  ****/
         //get shopping cart
-        ShoppingCart shoppingCart = restTemplate.getForObject(
-                shoppingCartServiceUrl + "/{cartNumber}", ShoppingCart.class, shoppingCartNumber);
-        System.out.println("----------- get shopping cart-----------------------");
-        System.out.println(shoppingCart);
+//        ShoppingCart shoppingCart = restTemplate.getForObject(
+//                shoppingCartServiceUrl + "/{cartNumber}", ShoppingCart.class, shoppingCartNumber);
+//        System.out.println("----------- get shopping cart-----------------------");
+//        System.out.println(shoppingCart);
+
+
 
         /**** 6. Delete one product from the shoppingcart ****/
-        restTemplate.delete(shoppingCartUrl + "/{cartId}/products/{productNumber}", shoppingCartNumber, product1.getProductNumber());
+//        restTemplate.delete(shoppingCartUrl + "/{cartId}/products/{productNumber}", shoppingCartNumber, product1.getProductNumber());
+
+
 
         /**** 7. Change the quantity of one of the products ****/
-        restTemplate.put(shoppingCartUrl + "/{cartId}/products/{productNumber}", shoppingCartNumber, product1.getProductNumber());
+//        restTemplate.put(shoppingCartUrl + "/{cartId}/products/{productNumber}", shoppingCartNumber, product1.getProductNumber());
+
+
 
         /**** 8. Retrieve and show the shoppingcart  ****/
-        ShoppingCart shoppingCart1 = restTemplate.getForObject(
-                shoppingCartServiceUrl + "/{cartNumber}", ShoppingCart.class, shoppingCartNumber);
-        System.out.println("----------- get shopping cart-----------------------");
-        System.out.println(shoppingCart1);
+//        ShoppingCart shoppingCart1 = restTemplate.getForObject(
+//                shoppingCartServiceUrl + "/{cartNumber}", ShoppingCart.class, shoppingCartNumber);
+//        System.out.println("----------- get shopping cart-----------------------");
+//        System.out.println(shoppingCart1);
+
+
 
         /**** 9. Checkout the shoppingcart  ****/
-        String checkoutMessage = restTemplate.getForObject(shoppingCartUrl + "/{cartId}/checkout",  String.class, shoppingCartNumber);
-        System.out.println(checkoutMessage);
+//        String checkoutMessage = restTemplate.getForObject(shoppingCartUrl + "/{cartId}/checkout",  String.class, shoppingCartNumber);
+//        System.out.println(checkoutMessage);
+
+
 
         /**** 11. Retrieve and show the order  ****/
         //retrieve order
-        Order order = restTemplate.getForObject(orderUrl + "/{orderNumber}", Order.class, "order1");
+//        Order order = restTemplate.getForObject(orderUrl + "/{orderNumber}", Order.class, "order1");
         //show order
-        System.out.println("----------- get order1-----------------------");
-        System.out.println(order);
+//        System.out.println("----------- get order1-----------------------");
+//        System.out.println(order);
 
     }
 
