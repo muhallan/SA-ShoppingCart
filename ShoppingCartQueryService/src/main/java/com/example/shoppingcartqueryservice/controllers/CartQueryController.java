01da1ff620ffa86974453b217d935b850212ef6f
Kaleab <kaleab.berhane12@gmail.com>
2022-03-16 01:24:17 -0500

    query service api updates and message fixing

--- ShoppingCartQueryService/src/main/java/com/example/shoppingcartqueryservice/controllers/CartQueryController.java
+++ ShoppingCartQueryService/src/main/java/com/example/shoppingcartqueryservice/controllers/CartQueryController.java
@@ -3,6 +3,8 @@ package com.example.shoppingcartqueryservice.controllers;
 import com.example.shoppingcartqueryservice.domain.ShoppingCart;
 import com.example.shoppingcartqueryservice.service.CartQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,22 @@ public class CartQueryController {
     private CartQueryService cartQueryService;
 
     @GetMapping("/{cartNumber}")
-    public ShoppingCart getCart(@PathVariable Long cartNumber){
-        return cartQueryService.getShoppingCart(cartNumber);
+    public ResponseEntity<?> getCart(@PathVariable Long cartNumber){
+        if(cartQueryService.getShoppingCart(cartNumber) !=null){
+            return new ResponseEntity<ShoppingCart>(cartQueryService.getShoppingCart(cartNumber), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<String>("COULDN'T GET CART", HttpStatus.BAD_REQUEST);
+        }
+
     }
-    @GetMapping
-    public List<ShoppingCart> getCarts(){
-        return cartQueryService.getShoppingCarts();
+    @GetMapping()
+    public ResponseEntity<?> getCart(){
+        if(cartQueryService.getShoppingCarts() !=null){
+            return new ResponseEntity<List<ShoppingCart>>(cartQueryService.getShoppingCarts(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<String>("COULDN'T GET CARTS", HttpStatus.BAD_REQUEST);
+        }
+
     }
+
 }
